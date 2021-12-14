2896d4892accc52deb256d083be4895ea0e0a82e
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-14 15:06:50 +0100

    Rješavanje trećeg zadatka

--- Edunova01/src/edunova/Z03.java
+++ Edunova01/src/edunova/Z03.java
@@ -1,10 +1,24 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Z03 {
 	
 	//Susjed kopa septičku jamu. Traži tebe da mu osiguraš odvoz zemlje.
 	//Napiši program koji za unesene podatke izračunava koliko
 	//kubika zemlje treba odvesti.
-
+	public static void main(String[] args) {
+	
+		double duzina = Double.parseDouble(JOptionPane.showInputDialog("Upiši dužinu septičke jame"));
+		
+		double sirina = Double.parseDouble(JOptionPane.showInputDialog("Upiši širinu septičke jame"));
+		
+		double dubina = Double.parseDouble(JOptionPane.showInputDialog("Upiši dubinu septičke jame"));
+		
+		double kubikaza = duzina * sirina * dubina;
+		
+		System.out.println(kubikaza);
+		
+	}
 
 }
