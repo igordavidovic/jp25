c83e18bff24516bbcc41365e8abce7ef48a14e9b
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-14 15:01:52 +0100

    Rješavanje prvog zadatka

--- Edunova01/src/edunova/Z01.java
+++ Edunova01/src/edunova/Z01.java
@@ -1,8 +1,18 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Z01 {
 	
 	// Program učitava dva cijela broja i ispisuje njihovu razliku
-
+	public static void main(String[] args) {
+		int prviCijeliBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi cijeli broj"));
+		
+		int drugiCijeliBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi cijeli broj"));
+		
+		int razlika = prviCijeliBroj - drugiCijeliBroj;
+		
+		System.out.println(razlika);
+	}
 
 }
