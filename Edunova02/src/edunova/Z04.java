cbbf05ac494ac50e65449f3074e52f3622b0786c
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-16 14:25:00 +0100

    Rješavanje četvrtog if zadatka

--- Edunova02/src/edunova/Z04.java
+++ Edunova02/src/edunova/Z04.java
@@ -1,5 +1,7 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Z04 {
 	
 	// Napišite program koji za danu širinu i dužinu igrališta ispisuje 
@@ -8,6 +10,24 @@ public class Z04 {
 	// Nogomet 6500 – 7000 m2
 	// Rukomet 800 m2
 	// Ragbi 7000 m2
-
+	public static void main(String[] args) {
+		
+		double sirina = Double.parseDouble(JOptionPane.showInputDialog("Upiši širinu igrališta"));
+	
+		double duzina = Double.parseDouble(JOptionPane.showInputDialog("Upiši dužinu igrališta"));
+		
+		double kvadratura = sirina * duzina;
+		
+		if(kvadratura >= 800 && kvadratura < 6500) {
+			System.out.println("Rukomet");
+		}else if(kvadratura >= 6500 && kvadratura < 7000) {
+			System.out.println("Rukomet i nogomet");
+		}else if(kvadratura >=7000) {
+			System.out.println("Rukomet, nogomet i ragbi");
+		}else {
+			System.out.println("Na zadanoj kvadraturi se ne igra niti jedan on ponuđenih sportova");
+		}
+		
+	}
 
 }
