94ea20b4ab3baba72ba4877ff6977c5a7692400f
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-16 14:11:31 +0100

    Rješavanje drugog if zadatka

--- Edunova02/src/edunova/Z02.java
+++ Edunova02/src/edunova/Z02.java
@@ -1,9 +1,24 @@
 package edunova;
 
+import javax.swing.JOptionPane;
 
 public class Z02 {
 
 	// Program unosi 3 cijela broja i ispisuje najmanji
-
+	public static void main(String[] args) {
+		int prviCijeliBroj = Integer.parseInt(JOptionPane.showInputDialog("Upiši prvi cijeli broj"));
+		
+		int drugiCijeliBroj = Integer.parseInt(JOptionPane.showInputDialog("Upiši drugi cijeli broj"));
+		
+		int treciCijeliBroj = Integer.parseInt(JOptionPane.showInputDialog("Upiši treći cijeli broj"));
+		
+		if(prviCijeliBroj <= drugiCijeliBroj && prviCijeliBroj <= treciCijeliBroj) {
+			System.out.println(prviCijeliBroj);
+		}else if(drugiCijeliBroj <= prviCijeliBroj && drugiCijeliBroj <= treciCijeliBroj) {
+			System.out.println(drugiCijeliBroj);
+		}else {
+			System.out.println(treciCijeliBroj);
+		}
+	}
 	
 }
