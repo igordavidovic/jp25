c4aee244c33c052fd37dafead17c689f49e8029a
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-16 14:14:24 +0100

    Rješavanje trećeg if zadatka

--- Edunova02/src/edunova/Z03.java
+++ Edunova02/src/edunova/Z03.java
@@ -1,9 +1,19 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Z03 {
 	
 	// Program unosi dvije logičke vrijednosti
 	// u slučaju da su obje istinite ispisuje DA
 	
-
+	public static void main(String[] args) {
+		boolean prvaVrijednost = Boolean.parseBoolean(JOptionPane.showInputDialog("True/False"));
+		
+		boolean drugaVrijednost = Boolean.parseBoolean(JOptionPane.showInputDialog("True/False"));
+		
+		if(prvaVrijednost == true && drugaVrijednost == true ) {
+			System.out.println("DA");
+		}
+	}
 }
