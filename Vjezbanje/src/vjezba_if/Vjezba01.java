023ef010c28691f1e64f0ec584ed4b795f75a03b
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-17 15:00:23 +0100

    Vježbanje if, drugi primjer

--- Vjezbanje/src/vjezba_if/Vjezba01.java
+++ Vjezbanje/src/vjezba_if/Vjezba01.java
@@ -5,16 +5,16 @@ import javax.swing.JOptionPane;
 public class Vjezba01 {
 	public static void main(String[] args) {
 		int broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
-		
-		if(broj > 0) {
+
+		if (broj > 0) {
 			System.out.println("Pozitivan");
-		}else if(broj < 0){
+		} else if (broj < 0) {
 			System.out.println("Negativan");
-		}else {
+		} else {
 			System.out.println("Broj je nula");
 		}
-		
+
 		System.out.println(broj > 0 ? "Pozitivan" : (broj < 0 ? "Negativan" : "Broj je nula"));
-		
+
 	}
 }
--- Vjezbanje/src/vjezba_if/Vjezba02.java
+++ Vjezbanje/src/vjezba_if/Vjezba02.java
@@ -1,5 +1,23 @@
 package vjezba_if;
 
-public class Vjezba02 {
+import javax.swing.JOptionPane;
 
+public class Vjezba02 {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Upiši prvi cijeli broj"));
+		
+		int b = Integer.parseInt(JOptionPane.showInputDialog("Upiši drugi cijeli broj"));
+		
+		int c = Integer.parseInt(JOptionPane.showInputDialog("Upiši treći cijeli broj"));
+		
+		if(a >= b && a >=c) {
+			System.out.println(a);
+		}else if(b >= a && b >= c) {
+			System.out.println(b);
+		}else {
+			System.out.println(c);
+		}
+		
+		System.out.println(a >= b && a >= c ? a :(b >= a && b >=c ? b : c));
+	}
 }
