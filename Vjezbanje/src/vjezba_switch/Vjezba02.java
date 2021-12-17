d735f348d07df06ca2f0b65d2c16677708e8f264
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-17 16:10:38 +0100

    Vježbanje switch, drugi primjer

--- Vjezbanje/src/vjezba_switch/Vjezba02.java
+++ Vjezbanje/src/vjezba_switch/Vjezba02.java
@@ -1,5 +1,41 @@
 package vjezba_switch;
 
-public class Vjezba02 {
+import javax.swing.JOptionPane;
 
+public class Vjezba02 {
+	public static void main(String[] args) {
+
+		int unos = Integer.parseInt(
+				JOptionPane.showInputDialog("Izaberi jedan od zadanih brojeva \n 1 \n 2 \n 3 \n 4 \n 5 \n 6"));
+
+		switch (unos) {
+		case 1:
+			char a1 = 'A';
+			System.out.println(a1);
+			break;
+		case 2:
+			char a2 = 'B';
+			System.out.println(a2);
+			break;
+		case 3:
+			char a3 = 'C';
+			System.out.println(a3);
+			break;
+		case 4:
+			char a4 = 'Č';
+			System.out.println(a4);
+			break;
+		case 5:
+			char a5 = 'Ć';
+			System.out.println(a5);
+			break;
+		case 6:
+			char a6 = 'D';
+			System.out.println(a6);
+			break;
+		default:
+			System.out.println("Nisi izabrao jedan od ponuđenih brojeva");
+			break;
+		}
+	}
 }
