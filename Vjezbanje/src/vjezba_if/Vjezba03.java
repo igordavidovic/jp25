133cab21c16c23df49b1def66016372bac569d61
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-17 15:27:35 +0100

    Vježba if , treći primjer

--- Vjezbanje/src/vjezba_if/Vjezba03.java
+++ Vjezbanje/src/vjezba_if/Vjezba03.java
@@ -5,21 +5,34 @@ import javax.swing.JOptionPane;
 public class Vjezba03 {
 	public static void main(String[] args) {
 		double a = Double.parseDouble(JOptionPane.showInputDialog("Upiši prvi decimalni broj"));
-		
-		if(a == 0 ) {
+
+		if (a == 0) {
 			System.out.println("Nula");
-		}else if(a < 0 && a > -1) {
+		} else if (a < 0 && a > -1) {
 			System.out.println("Negativan mali broj");
-		}else if(a < 0 && a <= -1 && a >= -1000000) {
+		} else if (a < 0 && a <= -1 && a >= -1000000) {
 			System.out.println("Negativan broj");
-		}else if(a < 0 && a < -1000000) {
+		} else if (a < 0 && a < -1000000) {
 			System.out.println("Negativan veliki broj");
-		}else if(a > 0 && a < 1) {
+		} else if (a > 0 && a < 1) {
 			System.out.println("Pozitivan mali broj");
-		}else if(a > 0 && a >= 1 && a <= 1000000) {
+		} else if (a > 0 && a >= 1 && a <= 1000000) {
 			System.out.println("Pozitivan broj");
-		}else{
+		} else {
 			System.out.println("Pozitivan veliki broj");
 		}
+
+		System.out
+				.println(
+						a == 0 ? "Nula"
+								: (a < 0 && a > -1 ? "Negativan mali broj"
+										: (a < 0 && a <= -1
+												&& a >= -1000000
+														? "Negativan broj"
+														: (a < 0 && a < -1000000 ? "Negativan veliki broj"
+																: (a > 0 && a < 1 ? "Pozitivan mali broj"
+																		: (a > 0 && a >= 1 && a <= 1000000
+																				? "Pozitivan broj"
+																				: "Pozitivan veliki broj"))))));
 	}
 }
