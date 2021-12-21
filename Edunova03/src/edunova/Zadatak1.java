3aeb8d9d1233669d9a11f66b336d385a816cb4ff
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-21 13:00:55 +0100

    Rješavanje prvog zadatka

--- Edunova03/src/edunova/Zadatak1.java
+++ Edunova03/src/edunova/Zadatak1.java
@@ -1,9 +1,30 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak1 {
 	
 	// Program učitava 3 broja u niz
 	// Ispisuje najveći
 	// Ne smijete koristiti niti jednu drugu varijablu osim deklariranog niza
-
+	public static void main(String[] args) {
+		
+		int[] niz = new int[3];
+		
+		niz[0] = Integer.parseInt(JOptionPane.showInputDialog("Upiši prvi cijeli broj"));
+		niz[1] = Integer.parseInt(JOptionPane.showInputDialog("Upiši drugi cijeli broj"));
+		niz[2] = Integer.parseInt(JOptionPane.showInputDialog("Upiši treći cijeli broj"));
+	
+		if(niz[0] >= niz[1] && niz[0] >= niz[2]) {
+			System.out.println(niz[0]);
+		}else if(niz[1] >= niz[0] && niz[1] >= niz[2]) {
+			System.out.println(niz[1]);
+		}else {
+			System.out.println(niz[2]);
+		}
+		
+		System.out.println(niz[0] >= niz[1] && niz[0] >= niz[2] ? niz[0] :(niz[1] >= niz[0] && niz[1] >= niz[2] ? niz[1] : niz[2]));
+		
+	}
+	
 }
