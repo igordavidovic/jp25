4b1b2ff647eaa80439fb4a0df51037cdb832d3a8
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-21 13:18:58 +0100

    Rješavanje trećeg zadatka

--- Edunova03/src/edunova/Zadatak3.java
+++ Edunova03/src/edunova/Zadatak3.java
@@ -4,6 +4,10 @@ public class Zadatak3 {
 	
 	// ispišite sve brojeve od 20 do 1 jedno ispod drugog
 	
-
+	public static void main(String[] args) {
+		for(int i = 20;i>1;i--) {
+			System.out.println(i);
+		}
+	}
 
 }
