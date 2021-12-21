f2677c033bf4dd01eef4a8f703722c998ce66263
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-21 13:08:44 +0100

    Rješavanje drugog zadatka

--- Edunova03/src/edunova/Zadatak2.java
+++ Edunova03/src/edunova/Zadatak2.java
@@ -1,10 +1,19 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak2 {
 	
 	// Program od korisnika traži unos dva cijela broja
 	// S vrijednostima unesenih brojeva deklarirajte matricu
 	
 	// primjer: Čovjek unese 5 i 6, vi napravite matricu 5x6 
-
+	public static void main(String[] args) {
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Upiši prvi cijeli broj"));
+		
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Upiši drugi cijeli broj"));
+		
+		int[][] matrica = new int[x][y];
+	}
 }
