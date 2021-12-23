fc7c14a1ed19703398feac828f6ad98ca7d018c0
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-23 13:53:00 +0100

    Rješavanje trećeg zadatka

--- Edunova04/src/edunova/Zadatak3.java
+++ Edunova04/src/edunova/Zadatak3.java
@@ -10,28 +10,35 @@ public class Zadatak3 {
 	// s svim ostalim brojevima između 1 i unesenog broja nije cjelobrjono djeljiv
 	
 	public static void main(String[] args) {
-		//int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
-		boolean prim=true;
-
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		boolean prim = true;
+		
+		for(int x = 2;x < broj;x++) {
+			prim = true;
+			if(broj%x==0) {
+				prim = false;
+				break;
+			}
+		}
+		System.out.println(broj + " " + (prim ? "je ": "nije ") + "prim broj " );
+		
+		//podzadatak
+		//Ispisati sve prim brojeve između 278 i 2455 
+		
 		for(int t=278; t<2455;t++) {
 			prim=true;
 			for(int i=2;i<t;i++) {
-				//System.out.println(broj + "%" + i + "=" +broj%i);
 				if(t%i==0) {
-					//System.out.println("Nije prim broj");
 					prim=false;
 					break;
 				}
 			}
-			//System.out.println(t + " " + (prim ? "je ": "nije ") + "prim broj " );
 			if(prim) {
 				System.out.println(t);
 			}
 		
 		}
 		
-		//podzadatak
-		//Ispisati sve prim brojeve između 278 i 2455 
 		
 		
 	}
