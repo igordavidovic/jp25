f82458bf9db3a00ac4cf45e4ec33286174f1bd09
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-23 14:20:15 +0100

    Rješavanje petog zadatka

--- Edunova04/src/edunova/Zadatak5.java
+++ Edunova04/src/edunova/Zadatak5.java
@@ -1,9 +1,23 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak5 {
 	
 	// Program učitava 5 cjelih brojeva
 	// foreach petljom ispisuje sve učitane brojeve
 	// ispisuje prosjek učitanih brojeva
-
+	public static void main(String[] args) {
+		int[] niz = new int[5];
+		int zbroj = 0;
+		for(int i = 0;i<niz.length;i++) {
+			niz[i]=Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+			zbroj+=niz[i];
+		}
+		for(int i = 0;i<niz.length;i++) {
+			System.out.println(niz[i]);
+		}
+		System.out.println();
+		System.out.println("Zbroj upisanih brojeva je " + (zbroj / niz.length));
+	}
 }
