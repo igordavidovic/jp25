7f9370ca368882d025436bdb5c4818fdc1c3de2c
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-23 13:39:25 +0100

    Rješavanje drugog zadatka

--- Edunova04/src/edunova/Zadatak2.java
+++ Edunova04/src/edunova/Zadatak2.java
@@ -1,15 +1,27 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak2 {
-	
+
 	// Za upisani cijeli broj ispisati samo prvu znamenku
-	
+
 	// 15262 -> 1
 	// 874 -> 8
-	
-	
-	// podzadatak: Ispisati broj na mjestu jedinice (zadnji broj)
-	// 15262 -> 2
-	// 874 -> 4
-
+	public static void main(String[] args) {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+		while (i > 10) {
+			i /= 10;
+		}
+		System.out.println(i);
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+		// podzadatak: Ispisati broj na mjestu jedinice (zadnji broj)
+		// 15262 -> 2
+		// 874 -> 4
+		while(x > 10) {
+			x %= 10;
+		}
+		System.out.println(x);
+	}
 }
