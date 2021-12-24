146ba4fd557307c4942fcecc875387a9c58a54ee
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-24 12:55:12 +0100

    Formatiranje koda

--- Basicpart1/src/vjezba/Vjezba01.java
+++ Basicpart1/src/vjezba/Vjezba01.java
@@ -22,25 +22,28 @@ public class Vjezba01 {
 		System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 
 		// #5
-		/*
-		 * int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj")); int y =
-		 * Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
-		 * 
-		 * System.out.println(x * y);
-		 * 
-		 * //#6 int j = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj")); int
-		 * k = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
-		 * 
-		 * System.out.println(j + k); System.out.println(j - k); System.out.println(j *
-		 * k); System.out.println(j / k); System.out.println(j % k);
-		 */
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+
+		System.out.println(x * y);
+
+		// #6
+		int j = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		int k = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+
+		System.out.println(j + k);
+		System.out.println(j - k);
+		System.out.println(j * k);
+		System.out.println(j / k);
+		System.out.println(j % k);
+
 		// #7
-		/*
-		 * int t = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
-		 * 
-		 * for (int i = 1; i <= 10; i++) { System.out.println(t + " * " + i + " = " + (t
-		 * * i)); }
-		 */
+
+		int t = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(t + " * " + i + " = " + (t * i));
+		}
 
 		// #11
 
@@ -52,93 +55,115 @@ public class Vjezba01 {
 		System.out.println("Perimeter " + "je " + perimeter);
 
 		// #12
-		/*
-		 * int broj1 =
-		 * Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj")); int broj2
-		 * = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj")); int
-		 * broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
-		 * 
-		 * int zbroj = broj1 + broj2 + broj3;
-		 * 
-		 * System.out.println(zbroj / 3);
-		 */
+
+		int broj1 = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+		int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+		int broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši cijeli broj"));
+
+		int zbroj = broj1 + broj2 + broj3;
+
+		System.out.println(zbroj / 3);
 
 		// #15
-		/*
-		 * int a = 10; int b = 15; int swap;
-		 * 
-		 * System.out.println("a " + "je " + a + " b " + "je " + b); swap = a; a = b; b
-		 * = swap; System.out.println("a " + "je "+ a + " b " + "je " + b);
-		 */
+
+		int a = 10;
+		int b = 15;
+		int swap;
+
+		System.out.println("a " + "je " + a + " b " + "je " + b);
+		swap = a;
+		a = b;
+		b = swap;
+		System.out.println("a " + "je " + a + " b " + "je " + b);
 
 		// #32
-		/*
-		 * int broj4 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj")); int
-		 * broj5 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
-		 * 
-		 * if(broj4 == broj5) { System.out.println(broj4 + " == " + broj5); } if(broj4
-		 * != broj5) { System.out.println(broj4 + " != " + broj5); } if(broj4 < broj5) {
-		 * System.out.println(broj4 + " < " + broj5); } if(broj4 > broj5) {
-		 * System.out.println(broj4 + " > " + broj5); }
-		 * 
-		 * //#33 int suma = 0; int broj6 =
-		 * Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
-		 * 
-		 * while(broj6 != 0) { suma += broj6 % 10; broj6 /= 10; }
-		 * System.out.println(suma);
-		 * 
-		 * //#39 int kolicina = 0; for(int i =1;i<=4;i++) { for(int x = 1;x<=4;x++) {
-		 * for (int y = 1; y <= 4;y++) { if(i != x && i != y && x !=y) {
-		 * System.out.println(i + "" + x + "" + y); kolicina++; } } }
-		 * 
-		 * } System.out.println(kolicina);
-		 */
+
+		int broj4 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+		int broj5 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+
+		if (broj4 == broj5) {
+			System.out.println(broj4 + " == " + broj5);
+		}
+		if (broj4 != broj5) {
+			System.out.println(broj4 + " != " + broj5);
+		}
+		if (broj4 < broj5) {
+			System.out.println(broj4 + " < " + broj5);
+		}
+		if (broj4 > broj5) {
+			System.out.println(broj4 + " > " + broj5);
+		}
+
+		// #33
+		int suma = 0;
+		int broj6 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+
+		while (broj6 != 0) {
+			suma += broj6 % 10;
+			broj6 /= 10;
+		}
+		System.out.println(suma);
+
+		// #39
+		int kolicina = 0;
+		for (int i = 1; i <= 4; i++) {
+			for (int z = 1; z <= 4; z++) {
+				for (int u = 1; u <= 4; y++) {
+					if (i != z && i != u && z != u) {
+						System.out.println(i + "" + z + "" + u);
+						kolicina++;
+					}
+				}
+			}
+
+		}
+		System.out.println(kolicina);
 
 		// #44
-		/*int br = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+		int br = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
 
 		System.out.println(br + " + " + br + "" + br + " + " + br + "" + br + "" + br);
-		*/
-		//#48
-/*		for(int i = 1;i<100;i++) {
-			if(i%2==0) {
+
+		// #48
+		for (int i = 1; i < 100; i++) {
+			if (i % 2 == 0) {
 				continue;
 			}
 			System.out.println(i);
 		}
 
-		//#49
-		
+		// #49
+
 		int parnepar = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
-		
-		if(parnepar % 2 == 0) {
+
+		if (parnepar % 2 == 0) {
 			System.out.println("1");
-		}else {
+		} else {
 			System.out.println("0");
 		}
-		*/
-		//#50
+
+		// #50
 		System.out.println("Djeljivi sa 3 :");
-		for(int i = 1;i < 100;i++) {
-			if(i % 3 == 0) {
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0) {
 				System.out.print(i + " ");
 			}
 		}
 		System.out.println();
 		System.out.println("Djeljivi sa 5 :");
-		for(int i = 1;i<100;i++) {
-			if(i % 5 == 0) {
+		for (int i = 1; i < 100; i++) {
+			if (i % 5 == 0) {
 				System.out.print(i + " ");
 			}
 		}
 		System.out.println();
 		System.out.println("Djeljivi sa 3 i 5:");
-	for(int i=1;i<100;i++) {
-		if(i % 3 == 0 && i % 5 == 0) {
-			System.out.print(i + " ");
+		for (int i = 1; i < 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.print(i + " ");
+			}
 		}
+		
 	}
-
-}
 
 }
---
+++ Basicpart1/src/vjezba/Vjezba02.java
@@ -0,0 +1,7 @@
+package vjezba;
+
+public class Vjezba02 {
+	public static void main(String[] args) {
+		
+	}
+}
