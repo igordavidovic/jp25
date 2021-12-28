9b760c7356eb7609ece5e2d7afbed28567f4ee0b
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-28 15:44:45 +0100

    Ugnježđivanje for petlji,dodavanje if-a

--- Ciklickatablica/src/zadaca/Tablica.java
+++ Ciklickatablica/src/zadaca/Tablica.java
@@ -7,35 +7,79 @@ import javax.swing.JOptionPane;
 public class Tablica {
 	public static void main(String[] args) {
 		int redak = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj redaka koji će biti u tablici"));
-		int redak1 = redak - 1;
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj stupaca koji će biti u tablici"));
-		int stupac1 = stupac - 1;
 		int[][] niz = new int [redak][stupac];
-		
+		int y1 = 0;
+		int x1 = 0;
 		int d = 1;
-
-		for(int y = stupac - 1; y >= 0;y--) {
-				niz[redak1][y] = d;
-				d++;			
-			}
-
-		for(int x = redak - 2;x >= 0;x--) {
-				niz[x][0] = d;
+		int proizvod = redak * stupac;
+		vanjska:
+		for(int i = 0;i < redak;i++) {
+		for(int y = stupac - (1+i) ; y >= (0+i);y--) {
+			if(d > proizvod) {
+			break vanjska;
+			}	
+			niz[redak - (1+i)][y] = d;
+			d++;			
+				}
+		for(int x = redak - (2+i);x > (0+i);x--) {
+			if(d > proizvod) {
+				break vanjska;
+			}		
+				niz[x][(0+i)] = d;
 				d++;
 			}
 		
-		for(int x = 1;x < stupac;x++) {
-				niz[0][x] = d;
-				d++;
-			
-		}
-		for(int x = 1;x < redak - 1 ;x++) {
-				niz[x][stupac - 1] = d;
+		for(int y = (0+i);y < stupac - i;y++) {
+			if(d > proizvod) {
+				break vanjska;
+			}		
+				niz[(0+i)][y] = d;
 				d++;
 		}
+		for(int x = (1+i);x < redak - (1+i) ;x++) {
+			if(d > proizvod) {
+				break vanjska;
+			}	
+				niz[x][stupac - (1+i)] = d;
+				d++;
+		}
+	}
+		//for int i = 1 dodaje se 1 na sve ove 
+
+	/*	for(int y = stupac - 2;y > 0;y--) {
+			if(d > proizvod) {
+				break;
+			}
+			niz[redak - 2][y] = d;
+			d++;
+		}
+		for(int x = redak - 3;x > 1;x--) {
+			if(d > proizvod) {
+				break;
+			}
+			niz[x][1] = d;
+			d++;
+		}
+		for(int y = 1;y < stupac - 1;y++) {
+			if(d > proizvod) {
+				break;
+			}
+			niz[1][y] = d;
+			d++;
+		}
+		for(int x = 2;x< redak - 2;x++) {
+			if(d > proizvod) {
+				break;
+			}
+			niz[x][stupac - 2] = d;
+			d++;
+		}
+	
+		//probati sa while i dve nove varijable
 		// prva četiri koja valjaju
 		// ova moram još
-		/*for(int i = stupac - 2;i > 0;i--) {
+		for(int i = stupac - 2;i > 0;i--) {
 			niz[redak - 2][i] = d;
 			d++;
 		}
