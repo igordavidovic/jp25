d3aa058231d3b9291f225b2bf51beeda6c317837
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-28 15:48:22 +0100

    Brisanje zakomenitranog,nepotrebnog i ispravljanje grešaka u pisanju

--- Ciklickatablica/src/zadaca/Tablica.java
+++ Ciklickatablica/src/zadaca/Tablica.java
@@ -9,8 +9,6 @@ public class Tablica {
 		int redak = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj redaka koji će biti u tablici"));
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj stupaca koji će biti u tablici"));
 		int[][] niz = new int [redak][stupac];
-		int y1 = 0;
-		int x1 = 0;
 		int d = 1;
 		int proizvod = redak * stupac;
 		vanjska:
@@ -45,58 +43,7 @@ public class Tablica {
 				d++;
 		}
 	}
-		//for int i = 1 dodaje se 1 na sve ove 
 
-	/*	for(int y = stupac - 2;y > 0;y--) {
-			if(d > proizvod) {
-				break;
-			}
-			niz[redak - 2][y] = d;
-			d++;
-		}
-		for(int x = redak - 3;x > 1;x--) {
-			if(d > proizvod) {
-				break;
-			}
-			niz[x][1] = d;
-			d++;
-		}
-		for(int y = 1;y < stupac - 1;y++) {
-			if(d > proizvod) {
-				break;
-			}
-			niz[1][y] = d;
-			d++;
-		}
-		for(int x = 2;x< redak - 2;x++) {
-			if(d > proizvod) {
-				break;
-			}
-			niz[x][stupac - 2] = d;
-			d++;
-		}
-	
-		//probati sa while i dve nove varijable
-		// prva četiri koja valjaju
-		// ova moram još
-		for(int i = stupac - 2;i > 0;i--) {
-			niz[redak - 2][i] = d;
-			d++;
-		}
-		for(int i = redak - 3;i > 0;i--) {
-			niz[i][1] = d;
-			d++;
-		}
-		for(int i = stupac - (stupac - 2);i < stupac - 1;i++) { //stupac - 2 // redak - 3  
-			niz[1][i] = d;
-			d++;
-		}*/
-		
-		
-		
-		
-		
-		
 		for(int i = 0; i < redak;i++){
 			System.out.println(Arrays.toString(niz[i]));
 		}
