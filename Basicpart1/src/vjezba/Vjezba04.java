bc83b6b1dcd2871e228003353c3aa47beeef06e4
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-12-30 17:17:14 +0100

    Vježbanje Basicpart-1

--- Basicpart1/src/vjezba/Vjezba04.java
+++ Basicpart1/src/vjezba/Vjezba04.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class Vjezba04 {
 	public static void main(String[] args) {
 		//#66
-	/*	int suma = 0;
+		int suma = 0;
 		int zbroj = 0;
 			vanjska:
 			for(int i = 2; suma < 100;i++) {
@@ -20,9 +20,9 @@ public class Vjezba04 {
 				suma++;
 		}
 			System.out.println(suma);
-			System.out.println(zbroj);*/
+			System.out.println(zbroj);
 		//#98 -- Mogu ga bolje napraviti
-/*		int broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+/*	int broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
 		int[] niz = new int [broj];
 		int suma1 = 0;
 		boolean prv = false;
@@ -50,7 +50,7 @@ public class Vjezba04 {
 		System.out.println(prv);
 		*/
 		//#99
-		/*int broj1 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+		int broj1 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int izbroj = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj koji će se tražiti"));
 		int suma1 = 0;
 		int[] niz1 = new int [broj1];
@@ -70,9 +70,9 @@ public class Vjezba04 {
 		}else {
 			System.out.println("False");
 		}
-		*/
+		
 		//#101
-		/*int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+		int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz2 = new int [broj2];
 		int suma2 = 0;
 		int suma3 = 0;
@@ -90,9 +90,9 @@ public class Vjezba04 {
 		}else {
 			System.out.println("Broj 10 se pojavljuje : " + suma2 + " puta");
 			System.out.println("Broj 20 se pojavljuje : " + suma3 + " puta");
-		}*/
+		}
 		//#102
-	/*	int broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+		int broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz3 = new int [broj3];
 		boolean prv1 = false;
 		for(int i = 0;i < niz3.length;i++) {
@@ -101,7 +101,7 @@ public class Vjezba04 {
 				prv1 = true;
 			}
 			}
-		System.out.println(prv1);*/
+		System.out.println(prv1);
 		//#103
 		int broj4 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz4 = new int [broj4];
--- Basicpart1/src/vjezba/Vjezba05.java
+++ Basicpart1/src/vjezba/Vjezba05.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class Vjezba05 {
 	public static void main(String[] args) {
 		//#104
-		/*int broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz = new int [broj];
 		for(int i = 0;i < niz.length;i++) {
 			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
@@ -28,7 +28,7 @@ public class Vjezba05 {
 				d--;
 		}
 		System.out.println(Arrays.toString(niz1));
-		*/
+		
 		//#105 Probati na drugi način
 	/*	int broj1 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz2 = new int [broj1];
@@ -42,7 +42,7 @@ public class Vjezba05 {
 		}*/
 		
 		//#106
-/*		int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+	int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz3 = new int [broj2];
 		for(int i = 0;i < niz3.length;i++) {
 			niz3[i] = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
@@ -53,10 +53,10 @@ public class Vjezba05 {
 		}
 		niz4[niz4.length-1] = niz3[0];
 		System.out.println(Arrays.toString(niz3));
-		System.out.println(Arrays.toString(niz4)); */
+		System.out.println(Arrays.toString(niz4)); 
 	
 		//#107
-	/*	int broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
+		int broj3 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko će brojeva biti u nizu"));
 		int[] niz5 = new int [broj3];
 		boolean prv = false;
 		for(int i = 0;i < niz5.length;i++) {
@@ -69,19 +69,19 @@ public class Vjezba05 {
 				break vanjska1;
 			}
 		}
-		System.out.println(prv);*/
+		System.out.println(prv);
 		
 		//#108
-	/*	int broj4 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+		int broj4 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
 		int suma = 0;
 		do {
 			int b4 = broj4 % 10;
 			suma += b4;
 			broj4 /= 10;
 		}while(broj4 > 0);
-		System.out.println(suma);*/
+		System.out.println(suma);
 		//#110
-	/*	double broj5 = Double.parseDouble(JOptionPane.showInputDialog("Upiši broj"));
+		double broj5 = Double.parseDouble(JOptionPane.showInputDialog("Upiši broj"));
 		while(broj5 > 4) {
 			broj5 /= 4;
 		}
@@ -89,9 +89,9 @@ public class Vjezba05 {
 			System.out.println("True");
 		}else {
 			System.out.println("False");
-		}*/
+		}
 		//#116
-/*		for(int i = 1;i <= 100;i++) {
+		for(int i = 1;i <= 100;i++) {
 			if(i % 3 == 0 && i % 5 == 0) {
 				System.out.println(i + " : Fizz buzz");
 			}else if(i % 5 == 0) {
@@ -99,9 +99,9 @@ public class Vjezba05 {
 			}else if(i % 3 == 0) {
 				System.out.println(i + " : Fizz");
 			}
-		}*/
+		}
 		//#119
-	/*	int broj6 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko dug će biti niz"));
+		int broj6 = Integer.parseInt(JOptionPane.showInputDialog("Upiši koliko dug će biti niz"));
 		int[] niz6 = new int[broj6];
 		for(int i = 0;i < niz6.length;i++) {
 			niz6[i] = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
@@ -111,14 +111,32 @@ public class Vjezba05 {
 			if(niz6[i] == se) {
 				System.out.println("Broj " + se + " se nalazi na " + (i + 1) + ". mjestu u nizu.");
 			}
-		}*/
+		}
 		//#120
 		int broj7 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj rijedaka koja će matrica imati"));
 		int broj8 = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj stupaca koja će matrica imati"));
 
 		int[][] niz7 = new int[broj7][broj8];
+		boolean prv1 = false;
+		for(int i = 0;i < broj7;i++) {
+			for(int j = 0;j < broj8;j++) {
+				niz7[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+			}
+		}
+		int tb = Integer.parseInt(JOptionPane.showInputDialog("Upiši broji koji će se tražiti u matrici"));
 		
-		
-	
+		for(int i = 0;i < broj7;i++) {
+			System.out.println(Arrays.toString(niz7[i]));
+		}
+		vanjska2:
+		for(int i = 0;i < broj7;i++) {
+			for(int j = 0;j < broj8;j++) {
+				if(niz7[i][j] == tb) {
+					prv1 = true;
+					break vanjska2;
+				}
+			}
+		}
+		System.out.println(prv1);
 	}
 }
