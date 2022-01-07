c2dcdc538bc36b7d6cc3c4160082f3dca17ac52c
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-07 16:54:13 +0100

    Vježbanje nizova

--- Javavjezbanje/src/vjezba/Vjezba17.java
+++ Javavjezbanje/src/vjezba/Vjezba17.java
@@ -2,10 +2,12 @@ package vjezba;
 
 import java.util.Arrays;
 
+import javax.swing.JOptionPane;
+
 public class Vjezba17 {
 	public static void main(String[] args) {
 		//#56
-	/*	int[] niz = { 0, 1, 1, 0, 1, 1, 0, 1, 0, 0 };
+		int[] niz = { 0, 1, 1, 0, 1, 1, 0, 1, 0, 0 };
 		for(int i = 0;i < niz.length;i++) {
 			for(int j = 0;j < niz.length;j++) {
 				if(niz[i] == 0) {
@@ -16,9 +18,9 @@ public class Vjezba17 {
 			}
 		}
 		System.out.println(Arrays.toString(niz));
-	*/
+	
 		//#58
-	/*	int[] A = { 1, 3, 5, 6, 7, 8, 10,12};
+		int[] A = { 1, 3, 5, 6, 7, 8, 10,12};
 		int[] B = { 2, 4, 9 , 11 ,13,15};
 		
 		for(int i = 0;i < B.length;i++) {
@@ -40,10 +42,10 @@ public class Vjezba17 {
 			}
 		}
 		System.out.println(Arrays.toString(A));
-		System.out.println(Arrays.toString(B));*/
+		System.out.println(Arrays.toString(B));
 		
 		//#59
-		/*int[] niz1 = { 2, 3, 5, 7, 10, -7, 5, 8, -5 ,9 , 1};
+		int[] niz1 = { 2, 3, 5, 7, 10, -7, 5, 8, -5 ,9 , 1};
 		int proizvod = 0;
 		for(int i = 0;i < niz1.length;i++) {
 			for(int j = i + 1;j < niz1.length;j++) {
@@ -54,10 +56,10 @@ public class Vjezba17 {
 				}
 			}
 		}
-		System.out.println(proizvod);*/
+		System.out.println(proizvod);
 		
 		//#63
-	/*	int[] nums1 = { 1, 2, 3, 4, 5, 6, 7};
+		int[] nums1 = { 1, 2, 3, 4, 5, 6, 7};
 		int[] nums2 = {0, 1, 2, 3, 4, 5, 6, 7};
 		
 		int[] rez1 = new int[nums1.length];
@@ -85,7 +87,7 @@ public class Vjezba17 {
 			}
 			rez2[i] = suma1;
 		}
-		System.out.println(Arrays.toString(rez2));*/
+		System.out.println(Arrays.toString(rez2));
 	
 		//#65
 		int[] niz2 = { 2, 3, 1, 7, 9, 5, 11, 3, 5 };
@@ -100,5 +102,68 @@ public class Vjezba17 {
 			}
 		}
 		System.out.println(minimum);
+		
+		//#66
+		int[] niz3 = {1, 2, -3, -4, 0, 6, 7, 8, 9};
+		int maximum = 0;
+		vanjska:
+		for(int i = 0;i < niz3.length;i++) {
+			int suma = 0;
+			suma += niz3[i];
+			for(int j = i+1;j < niz3.length;j++) {
+				suma += niz3[j];
+				if(suma <= 0) {
+					continue vanjska;
+				}
+			}
+			if(suma > maximum) {
+				maximum = suma;
+			}
+		}
+		System.out.println(maximum);
+		
+		//#71
+		int[] niz4 = {1, 2, 3, 0, 4, 6, -1, - 2};
+		int[] niz5 = new int[6];
+		for(int i = 0;i < niz4.length;i++) {
+			for(int j = 0;j < niz4.length;j++) {
+				if(niz4[i] > niz4[j]) {
+					int temp = niz4[j];
+					niz4[j] = niz4[i];
+					niz4[i] = temp;
+				}
+			}
+		}
+		for(int i = 0;i < niz5.length;i++) {
+			niz5[i] = niz4[i];
+		}
+		for(int i = 0;i < niz5.length;i++) {
+			System.out.print(niz5[i]);
+		}
+		
+		//#73
+		int[] niz6 =  { 3, 5, 6, 9, 8, 7 };
+		int[] niz7 = { 5, 0, 1, 2, 3, 4, -2 };
+		for(int i = 0;i < niz6.length;i++) {
+			for(int j = 0;j < niz6.length;j++) {
+				if(niz6[i] < niz6[j]) {
+					int temp = niz6[j];
+					niz6[j] = niz6[i];
+					niz6[i] = temp;
+				}
+			}
+		}
+		for(int i = 0;i < niz7.length;i++) {
+			for(int j = 0;j < niz7.length;j++) {
+				if(niz7[i] < niz7[j]) {
+					int temp = niz7[j];
+					niz7[j] = niz7[i];
+					niz7[i] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(niz6));
+		System.out.println(Arrays.toString(niz7));
+		
 	}
 }
