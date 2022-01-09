8960b01f875535f8f229c8f4e572c5f89e5a0e03
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-09 16:34:17 +0100

    Ispravljanje da se tablica kreće u smjeru kazaljke na satu

--- Ciklickatablica/src/zadaca/Alt2.java
+++ Ciklickatablica/src/zadaca/Alt2.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 
 public class Alt2 {
 	public static void main(String[] args) {
+		// Kreće iz gornjeg desnog kuta u smjeru kazaljke na satu
 		int rijedak = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj redaka koji će biti u tablici"));
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Upiši broj stupaca koji će biti u tablici"));
 		int[][] niz = new int [rijedak][stupac];
@@ -13,33 +14,33 @@ public class Alt2 {
 		int proizvod = rijedak * stupac;
 		vanjska:
 		for(int i = 0;i < rijedak;i++) {
-		for(int y = stupac - (1+i) ; y >= (0+i);y--) {
+		for(int x = 0+i ; x <= rijedak - (1+i);x++) {
 			if(d > proizvod) {
 			break vanjska;
 			}	
-			niz[0+i][y] = d;
+			niz[x][stupac - (1 + i)] = d;
 			d++;			
 				}
-		for(int x = 1+i;x < rijedak - (1+i);x++) {
+		for(int y = stupac - (2+i);y > (0+i);y--) {
+			if(d > proizvod) {
+				break vanjska;
+			}		
+				niz[rijedak - (1+i)][y] = d;
+				d++;
+			}
+		
+		for(int x = rijedak - (1+i);x > (0+i);x--) {
 			if(d > proizvod) {
 				break vanjska;
 			}		
 				niz[x][(0+i)] = d;
 				d++;
-			}
-		
-		for(int y = (0+i);y < stupac - i;y++) {
-			if(d > proizvod) {
-				break vanjska;
-			}		
-				niz[(rijedak - (1 + i))][y] = d;
-				d++;
 		}
-		for(int x = rijedak - (2+i);x > (0+i) ;x--) {
+		for(int y = (0+i);y < stupac - (1+i) ;y++) {
 			if(d > proizvod) {
 				break vanjska;
 			}	
-				niz[x][stupac - (1+i)] = d;
+				niz[(0+i)][y] = d;
 				d++;
 		}
 	}
