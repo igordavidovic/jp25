992efbda845c0465c8b6a90917a3a32c2db2328f
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-14 17:22:54 +0100

    Vježbanje metoda osmi zadatak

---
+++ Vjezbanjemetoda/src/vjezba/Vjezba08.java
@@ -0,0 +1,29 @@
+package vjezba;
+
+import java.util.Arrays;
+
+public class Vjezba08 {
+	private static int[] tempNiz = new int[2];
+	
+	private static boolean provjeraPrim(int a) {
+		if (a < 2) {
+			return false;
+		}
+		for(int i = 2; i < a;i++) {
+			if(a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 2; i < 100;i++) {
+			if(provjeraPrim(i) == true && provjeraPrim(i + 2) == true) {
+				tempNiz[0] = i;
+				tempNiz[1] = i + 2;
+				System.out.println(Arrays.toString(tempNiz));
+			}
+		}
+	}
+}
