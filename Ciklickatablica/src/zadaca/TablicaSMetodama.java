331a13ea0292066fc6326c656b253f988a7fdf89
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-15 15:21:46 +0100

    Mjenjanje sa private na package

--- Ciklickatablica/src/zadaca/TablicaSMetodama.java
+++ Ciklickatablica/src/zadaca/TablicaSMetodama.java
@@ -9,7 +9,7 @@ public class TablicaSMetodama {
 	private static int brojac = 1;	
 	private static int brojac1 = 0;
 	
-	private static int unesiBroj() {
+	static int unesiBroj() {
 		int a = 0;
 		while(true) {
 			a = Integer.parseInt(JOptionPane.showInputDialog("Unesi pozitivan broj"));
@@ -20,25 +20,25 @@ public class TablicaSMetodama {
 		return a;
 	}
 	
-	private static int[][] ucitanjeMatrice(int r,int s){
+	 static int[][] ucitanjeMatrice(int r,int s){
 		niz = new int[r][s];
 		
 		return niz;
 	}
 	
-	private static int proizvod() {
+	static int proizvod() {
 		int proizvod = niz.length * niz[0].length;
 		
 		return proizvod;
 	}
 	
-	private static void ispisMatrice() {
+	static void ispisMatrice() {
 		for(int i = 0;i < niz.length;i++) {
 		System.out.println(Arrays.toString(niz[i]));
 		}
 	}
 	
-	private static void dlDesnoLijevo() {
+	static void dlDesnoLijevo() {
 		for(int y = niz[0].length - (1+brojac1) ; y >= brojac1;y--) {
 			if(brojac > proizvod()) {
 			break;
@@ -48,7 +48,7 @@ public class TablicaSMetodama {
 		}
 	}
 	
-	private static void ddLijevoGore() {
+	static void ddLijevoGore() {
 		for(int x = niz.length - (2+brojac1);x > brojac1;x--) {
 			if(brojac > proizvod()) {
 				break;
@@ -58,7 +58,7 @@ public class TablicaSMetodama {
 		}
 	}
 	
-	private static void gdLijevoDesno() {
+	static void gdLijevoDesno() {
 		for(int y = brojac1;y < niz[0].length - brojac1;y++) {
 			if(brojac > proizvod()) {
 				break;
@@ -68,7 +68,7 @@ public class TablicaSMetodama {
 		}
 	}
 	
-	private static void glDesnoDolje() {
+	static void glDesnoDolje() {
 		for(int x = (1+brojac1);x < niz.length - (1+brojac1) ;x++) {
 			if(brojac > proizvod()) {
 				break;
