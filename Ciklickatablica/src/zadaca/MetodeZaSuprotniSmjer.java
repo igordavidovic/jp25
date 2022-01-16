72e473240fb2b2f50cc837844f7b4b77c9e4f704
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-16 15:46:10 +0100

    Tabele za suprotni smjer gornji lijevi i desni kut kut

--- Ciklickatablica/src/zadaca/MetodeZaSuprotniSmjer.java
+++ Ciklickatablica/src/zadaca/MetodeZaSuprotniSmjer.java
@@ -55,7 +55,7 @@ public class MetodeZaSuprotniSmjer {
 		}
 	}
 	
-	public static void suprotnaDesniDonjiKut() {
+	static void suprotnaDesniDonjiKut() {
 		TablicaSMetodama.ucitanjeMatrice(TablicaSMetodama.unesiBroj(), TablicaSMetodama.unesiBroj());
 		punjenjeTablice();
 		TablicaSMetodama.ispisMatrice();
@@ -82,6 +82,29 @@ public class MetodeZaSuprotniSmjer {
 			TablicaSMetodama.brojac++;
 		}
 	}
+	
+	static void gdSuprotno1() {
+		for (int y = TablicaSMetodama.niz[0].length
+				- (1 + TablicaSMetodama.brojac1); y > TablicaSMetodama.brojac1; y--) {
+			if (TablicaSMetodama.brojac > TablicaSMetodama.proizvod()) {
+				break;
+			}
+			TablicaSMetodama.niz[TablicaSMetodama.brojac1][y] = TablicaSMetodama.brojac;
+			TablicaSMetodama.brojac++;
+		}
+	}
+	
+	static void ddSuprotno1() {
+		for (int x = TablicaSMetodama.niz.length - (1 + TablicaSMetodama.brojac1); x > TablicaSMetodama.brojac1; x--) {
+			if (TablicaSMetodama.brojac > TablicaSMetodama.proizvod()) {
+				break;
+			}
+			TablicaSMetodama.niz[x][TablicaSMetodama.niz[0].length
+					- (1 + TablicaSMetodama.brojac1)] = TablicaSMetodama.brojac;
+			TablicaSMetodama.brojac++;
+		}
+	}
+	
 	//Suprotna iz lijevog kuta donjeg
 	static void punjenjeTablice1() {
 		while(TablicaSMetodama.brojac <= TablicaSMetodama.proizvod()) {
@@ -93,9 +116,41 @@ public class MetodeZaSuprotniSmjer {
 		}
 	}
 	
-	public static void suprotnaDonjiLijeviKut() {
+	static void suprotnaDonjiLijeviKut() {
 		TablicaSMetodama.ucitanjeMatrice(TablicaSMetodama.unesiBroj(), TablicaSMetodama.unesiBroj());
 		punjenjeTablice1();
+		TablicaSMetodama.ispisMatrice();
+	}
+	
+	static void punjenjeTablice2() {
+		while(TablicaSMetodama.brojac <= TablicaSMetodama.proizvod()) {
+		glSuprotno1();
+		dlSuprotno1();
+		ddSuprotno();
+		gdSuprotno();
+		TablicaSMetodama.brojac1++;
+		}		
+	}
+	
+	static void suprotnaGornjiLijeviKut() {
+		TablicaSMetodama.ucitanjeMatrice(TablicaSMetodama.unesiBroj(), TablicaSMetodama.unesiBroj());
+		punjenjeTablice2();
+		TablicaSMetodama.ispisMatrice();
+	}
+	
+	static void punjenjeTablice3() {
+		while(TablicaSMetodama.brojac <= TablicaSMetodama.proizvod()) {
+			gdSuprotno1();
+			glSuprotno();
+			dlSuprotno();
+			ddSuprotno1();
+			TablicaSMetodama.brojac1++;
+		}		
+	}
+	
+	static void suprotnaGornjiDesniKut() {
+		TablicaSMetodama.ucitanjeMatrice(TablicaSMetodama.unesiBroj(), TablicaSMetodama.unesiBroj());
+		punjenjeTablice3();
 		TablicaSMetodama.ispisMatrice();
 	}
 }
