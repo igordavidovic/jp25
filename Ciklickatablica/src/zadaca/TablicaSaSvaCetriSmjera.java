9e388186f81648e7f92bad6870ed0a1b9d2504d7
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-16 15:46:30 +0100

    Dodavanje izabera smjera kojim će matrica ići

--- Ciklickatablica/src/zadaca/TablicaSaSvaCetriSmjera.java
+++ Ciklickatablica/src/zadaca/TablicaSaSvaCetriSmjera.java
@@ -21,19 +21,39 @@ public class TablicaSaSvaCetriSmjera {
 
 	private static void izborSmjera() {
 		int izbor;
+		int izbor1;
 		while (true) {
+			izbor1 = Integer.parseInt(JOptionPane.showInputDialog(
+					"Izaberi u kojem smjeru će tablica ići \n1. U smjeru kazaljke na satu \n2. Suprotno smjera kazaljke na satu"));
+			if (izbor1 != 1 && izbor1 != 2) {
+				continue;
+				}
 			izbor = Integer.parseInt(JOptionPane.showInputDialog(
 					"Upisom broja izaberi odakle će tablica krenuti : \n1. Donji desni kut \n2. Donji lijevi kut \n3. Gornji desni kut \n4. Gornji lijevi kut "));
-			if (izbor == 1) {
-				donjiDesniKut();
-			} else if (izbor == 2) {
-				donjiLijeviKut();
-			} else if (izbor == 3) {
-				gornjiDesniKut();
-			} else if (izbor == 4) {
-				gornjiLijeviKut();
+			if (izbor1 == 1) {
+				if (izbor == 1) {
+					donjiDesniKut();
+				} else if (izbor == 2) {
+					donjiLijeviKut();
+				} else if (izbor == 3) {
+					gornjiDesniKut();
+				} else if (izbor == 4) {
+					gornjiLijeviKut();
+				} else {
+					continue;
+				}
 			} else {
-				continue;
+				if (izbor == 1) {
+					MetodeZaSuprotniSmjer.suprotnaDesniDonjiKut();
+				} else if (izbor == 2) {
+					MetodeZaSuprotniSmjer.suprotnaDonjiLijeviKut();
+				} else if (izbor == 3) {
+					MetodeZaSuprotniSmjer.suprotnaGornjiDesniKut();
+				} else if (izbor == 4) {
+					MetodeZaSuprotniSmjer.suprotnaGornjiLijeviKut();
+				} else {
+					continue;
+				}
 			}
 			break;
 		}
