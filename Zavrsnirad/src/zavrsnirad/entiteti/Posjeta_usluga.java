25aac5ec527cd99ca82e77e44b337be451e694bb
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-27 17:01:52 +0100

    Kreiranje klase posjeta_djelatnik

---
+++ Zavrsnirad/src/zavrsnirad/entiteti/Posjeta_usluga.java
@@ -0,0 +1,21 @@
+package zavrsnirad.entiteti;
+
+public class Posjeta_usluga {
+	private Usluga usluga;
+	private Posjeta posjeta;
+	
+	public Usluga getUsluga() {
+		return usluga;
+	}
+	public void setUsluga(Usluga usluga) {
+		this.usluga = usluga;
+	}
+	public Posjeta getPosjeta() {
+		return posjeta;
+	}
+	public void setPosjeta(Posjeta posjeta) {
+		this.posjeta = posjeta;
+	}
+	
+	
+}
