8fbbafcfcffd000e39e2fde277452629e1bb7184
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-27 16:25:01 +0100

    Kreiranje klase djelatnik

---
+++ Zavrsnirad/src/zavrsnirad/Djelatnik.java
@@ -0,0 +1,44 @@
+package zavrsnirad;
+
+import java.math.BigDecimal;
+
+public class Djelatnik {
+	private String ime;
+	private String prezime;
+	private String uloga;
+	private String email;
+	private BigDecimal placa;
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getUloga() {
+		return uloga;
+	}
+	public void setUloga(String uloga) {
+		this.uloga = uloga;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public BigDecimal getPlaca() {
+		return placa;
+	}
+	public void setPlaca(BigDecimal placa) {
+		this.placa = placa;
+	}
+	
+	
+}
