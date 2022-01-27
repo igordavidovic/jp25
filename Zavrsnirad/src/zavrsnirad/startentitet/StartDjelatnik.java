08aa48a241ecb252935efd2ea5eed34ddd56e599
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-27 17:43:58 +0100

    Dodavanje metoda za akcije i izbornika

--- Zavrsnirad/src/zavrsnirad/startentitet/StartDjelatnik.java
+++ Zavrsnirad/src/zavrsnirad/startentitet/StartDjelatnik.java
@@ -10,11 +10,12 @@ public class StartDjelatnik {
 	private List<Djelatnik> djelatnici;
 	private Scanner scanner;
 	private Djelatnik djelatnik;
-	
+
 	public StartDjelatnik() {
 		djelatnici = new ArrayList<>();
 		scanner = new Scanner(System.in);
 		program();
+		scanner.close();
 	}
 
 	private void program() {
@@ -23,8 +24,73 @@ public class StartDjelatnik {
 	}
 
 	private void izbornik() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("\n1. Unos mobitela");
+		System.out.println("2. Promjena mobitela");
+		System.out.println("3. Brisanje mobitela");
+		System.out.println("4. Pregled mobitela");
+		System.out.println("5. Izlaz iz programa");
+		switch (Unos.unesiInt(scanner, "Izaberi akciju")) {
+		case 1:
+			unos();
+			break;
+		case 2:
+			promjena();
+			break;
+		case 3:
+			brisanje();
+			break;
+		case 4:
+			pregled();
+			izbornik();
+			break;
+		case 5:
+			izlaz();
+			break;
+		default:
+			System.out.println("Izabrana akcija nije moguća");
+			izbornik();
+		}
+	}
+
+	private void izlaz() {
+
+	}
+
+	private void pregled() {
+		int a = 1;
+		for (Djelatnik d : djelatnici) {
+			System.out.println(a++ + ". " + d);
+		}
+
+	}
+
+	private void brisanje() {
+		pregled();
+		djelatnici.remove(Unos.unesiInt(scanner, "Unesi redni broj djelatnika koji će se ukloniti") - 1);
+
+	}
+
+	private void promjena() {
+		pregled();
+		int a = Unos.unesiInt(scanner, "Unesi redni broj djelatnika koji želite mjenjati");
+		djelatnik = djelatnici.get(a - 1);
+		djelatnik.setIme(Unos.unesiString(scanner, "Unesi novo ime"));
+		djelatnik.setPrezime(Unos.unesiString(scanner, "Unesi novo prezime"));
+		djelatnik.setUloga(Unos.unesiString(scanner, "Unesi novu ulogu"));
+		djelatnik.setEmail(Unos.unesiString(scanner, "Unesi novi email"));
+		djelatnik.setPlaca(Unos.unesiBigDecimal(scanner, "Unesi novu placu"));
+		izbornik();
+	}
+
+	private void unos() {
+		djelatnik = new Djelatnik();
+		djelatnik.setIme(Unos.unesiString(scanner, "Unesi ime djelatnika"));
+		djelatnik.setPrezime(Unos.unesiString(scanner, "Unesi prezime djelatnika"));
+		djelatnik.setUloga(Unos.unesiString(scanner, "Unesi uloga koju djelatnik obavlja"));
+		djelatnik.setEmail(Unos.unesiString(scanner, "Unesi email djelatnika"));
+		djelatnik.setPlaca(Unos.unesiBigDecimal(scanner, "Unesi placu djelatnika"));
+		djelatnici.add(djelatnik);
+		izbornik();
 	}
 
 	private void naslov() {
@@ -32,6 +98,8 @@ public class StartDjelatnik {
 		System.out.println("**** Djelatnik V1 ****");
 		System.out.println("**********************");
 	}
-	
-	
+
+	public static void main(String[] args) {
+		new StartDjelatnik();
+	}
 }
