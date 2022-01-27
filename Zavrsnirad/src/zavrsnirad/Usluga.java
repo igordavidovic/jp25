07a5993e9fa9d164f86566f1517af5f4a9e7c884
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-27 16:24:02 +0100

    Kreiranje klase usluga

---
+++ Zavrsnirad/src/zavrsnirad/Usluga.java
@@ -0,0 +1,30 @@
+package zavrsnirad;
+
+import java.math.BigDecimal;
+
+public class Usluga {
+	private Djelatnik djelatnik;
+	private String naziv;
+	private BigDecimal cijena;
+	
+	public Djelatnik getDjelatnik() {
+		return djelatnik;
+	}
+	public void setDjelatnik(Djelatnik djelatnik) {
+		this.djelatnik = djelatnik;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public BigDecimal getCijena() {
+		return cijena;
+	}
+	public void setCijena(BigDecimal cijena) {
+		this.cijena = cijena;
+	}
+	
+	
+}
