4e20eee4f7ff9794dd84e0a6b36191d1ae294b63
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-01-29 13:57:39 +0100

    Dodavanje konsturktora

--- Zavrsnirad/src/zavrsnirad/entiteti/Posjeta_usluga.java
+++ Zavrsnirad/src/zavrsnirad/entiteti/Posjeta_usluga.java
@@ -3,19 +3,30 @@ package zavrsnirad.entiteti;
 public class Posjeta_usluga {
 	private Usluga usluga;
 	private Posjeta posjeta;
-	
+
+	public Posjeta_usluga() {
+
+	}
+
+	public Posjeta_usluga(Usluga usluga, Posjeta posjeta) {
+		this.usluga = usluga;
+		this.posjeta = posjeta;
+	}
+
 	public Usluga getUsluga() {
 		return usluga;
 	}
+
 	public void setUsluga(Usluga usluga) {
 		this.usluga = usluga;
 	}
+
 	public Posjeta getPosjeta() {
 		return posjeta;
 	}
+
 	public void setPosjeta(Posjeta posjeta) {
 		this.posjeta = posjeta;
 	}
-	
-	
+
 }
