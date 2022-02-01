47951b9d05c8aeba86d1e4cb15ee26560990ff87
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-01 16:36:06 +0100

    Dodavanje try i catcha

--- Zavrsnirad/src/zavrsnirad/startentitet/Unos.java
+++ Zavrsnirad/src/zavrsnirad/startentitet/Unos.java
@@ -5,42 +5,55 @@ import java.util.Scanner;
 
 public class Unos {
 	public static int unesiInt(Scanner ulaz, String opis) {
-		int i=0;
-		while(true) {
-			System.out.print(opis + ": ");
-			i = Integer.parseInt(ulaz.nextLine());
-			if(i<1) {
-				System.out.println("Broj mora biti pozitivan");
-				continue;
+		int i = 0;
+		while (true) {
+			try {
+				System.out.print(opis + ": ");
+				i = Integer.parseInt(ulaz.nextLine());
+				if (i < 1) {
+					System.out.println("Broj mora biti pozitivan");
+					continue;
+				}
+				return i;
+			} catch (Exception e) {
+				System.out.println("Greška : " + e.getMessage());
 			}
-			return i;
+
 		}
 	}
-	
-	public static BigDecimal unesiBigDecimal(Scanner ulaz,String opis) {
-		BigDecimal i=BigDecimal.ZERO;
-		while(true) {
-			System.out.print(opis + ": ");
-			i = new BigDecimal(ulaz.nextLine());
-			if(i.compareTo(BigDecimal.ONE) < 0) {
-				System.out.println("Broj mora biti pozitivan");
-				continue;
+
+	public static BigDecimal unesiBigDecimal(Scanner ulaz, String opis) {
+		BigDecimal i = BigDecimal.ZERO;
+		while (true) {
+			try {
+				System.out.print(opis + ": ");
+				i = new BigDecimal(ulaz.nextLine());
+				if (i.compareTo(BigDecimal.ONE) < 0) {
+					System.out.println("Broj mora biti pozitivan");
+					continue;
+				}
+				return i;
+			} catch (Exception e) {
+				System.out.println("Greška : " + e.getMessage());
 			}
-			return i;
 		}
 	}
-	
-	
+
 	public static String unesiString(Scanner ulaz, String opis) {
-		String s="";
-		while(true) {
-			System.out.print(opis + ": ");
-			s = ulaz.nextLine();
-			if(s.length()==0) {
-				System.out.println( "Obavezan unos");
-				continue;
+		String s = "";
+		while (true) {
+			try {
+				System.out.print(opis + ": ");
+				s = ulaz.nextLine();
+				if (s.length() == 0) {
+					System.out.println("Obavezan unos");
+					continue;
+				}
+				return s;
+			} catch (Exception e) {
+				System.out.println("Greška : " + e.getMessage());
 			}
-			return s;
+
 		}
 	}
 }
