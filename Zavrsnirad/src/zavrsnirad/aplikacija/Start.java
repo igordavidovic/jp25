a922f50977057c540cc64a70c51e835e8544a1ad
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-04 17:25:06 +0100

    Dodavanje metoda za rad s korisnikom

--- Zavrsnirad/src/zavrsnirad/aplikacija/Start.java
+++ Zavrsnirad/src/zavrsnirad/aplikacija/Start.java
@@ -1,6 +1,7 @@
 package zavrsnirad.aplikacija;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,7 +17,11 @@ public class Start {
 	private List<Usluga> usluge;
 	private List<Posjeta> posjete;
 	private Scanner scanner;
-	
+	private Korisnik korisnik;
+	private Djelatnik djelatnik;
+	private Usluga usluga;
+	private Posjeta posjeta;
+
 	public Start() {
 		korisnici = new ArrayList<>();
 		djelatnici = new ArrayList<>();
@@ -29,7 +34,7 @@ public class Start {
 	}
 
 	private void glavniIzbornik() {
-		System.out.println("1. Rad s korisnicima");
+		System.out.println("\n1. Rad s korisnicima");
 		System.out.println("2. Rad s djelatnicima");
 		System.out.println("3. Rad s uslugama");
 		System.out.println("4. Rad s posjetama");
@@ -40,7 +45,75 @@ public class Start {
 		case 3 -> uslugaIzbornik();
 		case 4 -> posjetaIzbornik();
 		}
-		
+
+	}
+
+	private void posjetaIzbornik() {
+
+	}
+
+	private void uslugaIzbornik() {
+
+	}
+
+	private void djelatnikIzbornik() {
+
+	}
+
+	private void korisnikIzbornik() {
+		System.out.println("\n1. Pregled korisnika");
+		System.out.println("2. Unos novog korisnika");
+		System.out.println("3. Promjena korisnika");
+		System.out.println("4. Brisanje korisnika");
+		System.out.println("5. Povratak na glavni izbornik");
+		switch (Unos.unesiInt(scanner, "Odaberi akciju", 1, 5)) {
+		case 1 -> {
+			korisnikPregled();
+			korisnikIzbornik();
+		}
+		case 2 -> korisnikUnos();
+		case 3 -> korisnikPromjena();
+		case 4 -> korisnikBrisanje();
+		case 5 -> glavniIzbornik();
+		}
+	}
+
+	private void korisnikBrisanje() {
+		korisnikPregled();
+		korisnici.remove(Unos.unesiInt(scanner, "Unesite redni broj korisnika kojega želite izbrisati")-1);
+		korisnikIzbornik();
+	}
+
+	private void korisnikPromjena() {
+		korisnikPregled();
+		int a = Unos.unesiInt(scanner, "Unesite redni broj korisnika kojega želite mjenjati");
+		korisnik = korisnici.get(a-1);
+		korisnik.setSifra(Unos.unesiInt(scanner, "Unesite novu šifru korisnika"));
+		korisnik.setIme(Unos.unesiString(scanner, "Unesite novu ime korisnika"));
+		korisnik.setPrezime(Unos.unesiString(scanner, "Unesite novu prezime korisnika"));
+		korisnik.setEmail(Unos.unesiString(scanner, "Unesite novi email korisnika"));
+		korisnik.setOib(Unos.unesiString(scanner, "Unesite novi OIB korisnika"));
+		korisnik.setDatumRodenja(new Date());
+		korisnikIzbornik();
+	}
+
+	private void korisnikUnos() {
+		korisnik = new Korisnik();
+		korisnik.setSifra(Unos.unesiInt(scanner, "Unesite šifru korisnika"));
+		korisnik.setIme(Unos.unesiString(scanner, "Unesite ime korisnika"));
+		korisnik.setPrezime(Unos.unesiString(scanner, "Unesite prezime korisnika"));
+		korisnik.setEmail(Unos.unesiString(scanner, "Unesite email korisnika"));
+		korisnik.setOib(Unos.unesiString(scanner, "Unesite OIB korisnika"));
+		korisnik.setDatumRodenja(new Date());
+		korisnici.add(korisnik);
+		korisnikIzbornik();
+	}
+
+	private void korisnikPregled() {
+		int a = 1;
+		for (Korisnik k : korisnici) {
+			System.out.println(a++ + ". " + k);
+		}
 	}
 
 	private void naslov() {
@@ -48,6 +121,10 @@ public class Start {
 		System.out.println("****** Recepcija APP ******");
 		System.out.println("***************************");
 		System.out.println();
-		
+
+	}
+	
+	public static void main(String[] args) {
+		new Start();
 	}
 }
