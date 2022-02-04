85275ae59e92a8e218b3ee6d576bfb0f2f4a4697
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-04 17:34:11 +0100

    Dodavanje metoda za rad s djelatnikom

--- Zavrsnirad/src/zavrsnirad/aplikacija/Start.java
+++ Zavrsnirad/src/zavrsnirad/aplikacija/Start.java
@@ -57,7 +57,59 @@ public class Start {
 	}
 
 	private void djelatnikIzbornik() {
+		System.out.println("\n1. Pregled djelatnika");
+		System.out.println("2. Unos novog djelatnika");
+		System.out.println("3. Promjena djelatnika");
+		System.out.println("4. Brisanje djelatnika");
+		System.out.println("5. Povratak na glavni izbornik");
+		switch (Unos.unesiInt(scanner, "Odaberi akciju", 1, 5)) {
+		case 1 -> {
+			djelatnikPregled();
+			djelatnikIzbornik();
+		}
+		case 2 -> djelatnikUnos();
+		case 3 -> djelatnikPromjena();
+		case 4 -> djelatnikBrisanje();
+		case 5 -> glavniIzbornik();
+		}
+	}
 
+	private void djelatnikUnos() {
+		djelatnik = new Djelatnik();
+		djelatnik.setSifra(Unos.unesiInt(scanner, "Unesite šifru djelatnika"));
+		djelatnik.setIme(Unos.unesiString(scanner, "Unesite ime djelatnika"));
+		djelatnik.setPrezime(Unos.unesiString(scanner, "Unesite prezime djelatnika"));
+		djelatnik.setUloga(Unos.unesiString(scanner, "Unesite ulogu djelatnika"));
+		djelatnik.setEmail(Unos.unesiString(scanner, "Unesite email djelatnika"));
+		djelatnik.setPlaca(Unos.unesiBigDecimal(scanner, "Unesite plaću djelatnika"));
+		djelatnici.add(djelatnik);
+		djelatnikIzbornik();
+	}
+
+	private void djelatnikBrisanje() {
+		djelatnikPregled();
+		djelatnici.remove(Unos.unesiInt(scanner, "Unesite redni broj djelatnika kojega želite izbristati")-1);
+		djelatnikIzbornik();
+	}
+
+	private void djelatnikPromjena() {
+		djelatnikIzbornik();
+		int a = Unos.unesiInt(scanner, "Unesite redni broj djelatnika kojega želite mjenjati");
+		djelatnik = djelatnici.get(a - 1);
+		djelatnik.setSifra(Unos.unesiInt(scanner, "Unesite novu šifru djelatnika"));
+		djelatnik.setIme(Unos.unesiString(scanner, "Unesite novo ime djelatnika"));
+		djelatnik.setPrezime(Unos.unesiString(scanner, "Unesite nov prezime djelatnika"));
+		djelatnik.setUloga(Unos.unesiString(scanner, "Unesite novu ulogu djelatnika"));
+		djelatnik.setEmail(Unos.unesiString(scanner, "Unesite novi email djelatnika"));
+		djelatnik.setPlaca(Unos.unesiBigDecimal(scanner, "Unesite novu plaću djelatnika"));
+		djelatnikIzbornik();
+	}
+
+	private void djelatnikPregled() {
+		int a = 1;
+		for (Djelatnik d : djelatnici) {
+			System.out.println(a++ + ". " + d);
+		}
 	}
 
 	private void korisnikIzbornik() {
@@ -80,17 +132,17 @@ public class Start {
 
 	private void korisnikBrisanje() {
 		korisnikPregled();
-		korisnici.remove(Unos.unesiInt(scanner, "Unesite redni broj korisnika kojega želite izbrisati")-1);
+		korisnici.remove(Unos.unesiInt(scanner, "Unesite redni broj korisnika kojega želite izbrisati") - 1);
 		korisnikIzbornik();
 	}
 
 	private void korisnikPromjena() {
 		korisnikPregled();
 		int a = Unos.unesiInt(scanner, "Unesite redni broj korisnika kojega želite mjenjati");
-		korisnik = korisnici.get(a-1);
+		korisnik = korisnici.get(a - 1);
 		korisnik.setSifra(Unos.unesiInt(scanner, "Unesite novu šifru korisnika"));
-		korisnik.setIme(Unos.unesiString(scanner, "Unesite novu ime korisnika"));
-		korisnik.setPrezime(Unos.unesiString(scanner, "Unesite novu prezime korisnika"));
+		korisnik.setIme(Unos.unesiString(scanner, "Unesite novo ime korisnika"));
+		korisnik.setPrezime(Unos.unesiString(scanner, "Unesite novo prezime korisnika"));
 		korisnik.setEmail(Unos.unesiString(scanner, "Unesite novi email korisnika"));
 		korisnik.setOib(Unos.unesiString(scanner, "Unesite novi OIB korisnika"));
 		korisnik.setDatumRodenja(new Date());
@@ -123,7 +175,7 @@ public class Start {
 		System.out.println();
 
 	}
-	
+
 	public static void main(String[] args) {
 		new Start();
 	}
