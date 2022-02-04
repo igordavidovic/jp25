ba596f30908b1db0d272badaabddf8477193ba32
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-04 16:40:07 +0100

    Dodavanje konstruktora za entitet

--- Zavrsnirad/src/zavrsnirad/model/Entitet.java
+++ Zavrsnirad/src/zavrsnirad/model/Entitet.java
@@ -2,7 +2,15 @@ package zavrsnirad.model;
 
 public abstract class Entitet {
 	private int sifra;
-
+	
+	public Entitet() {
+		
+	}
+	
+	public Entitet(int sifra) {
+		this.sifra = sifra;
+	}
+	
 	public int getSifra() {
 		return sifra;
 	}
