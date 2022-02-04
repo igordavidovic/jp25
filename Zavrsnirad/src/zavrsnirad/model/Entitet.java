b511acf1c956f218e6863b34336e15674cfd3c51
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-04 16:31:39 +0100

    Dodavanje posebne klase za entitet koja sadrži šifru

---
+++ Zavrsnirad/src/zavrsnirad/model/Entitet.java
@@ -0,0 +1,15 @@
+package zavrsnirad.model;
+
+public abstract class Entitet {
+	private int sifra;
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	
+	
+}
